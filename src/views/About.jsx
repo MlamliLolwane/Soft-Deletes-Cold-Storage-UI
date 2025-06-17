5ccf11d2ec1a77e35a9cddfc350e7189c32f6c97
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-18 01:54:30 +0200

    edit: add responsiveness

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -4,23 +4,25 @@ function About() {
     return (
         <>
             <Navbar />
-            <div className="border m-5 p-4">
-                <h4>About</h4>
-                <p className="fw-lighter">
-                    This is a demo project showcasing the implementation of soft deletes based on 
-                    "This article".
+            <main className="ms-0">
+                <div className="border m-5 p-4">
+                    <h4>About</h4>
+                    <p className="fw-lighter">
+                        This is a demo project showcasing the implementation of soft deletes based on
+                        "This article".
 
-                    This project was inspired by a tweet from "Nuno Maduro" so I thought I would 
-                    showcase my take
-                    on the topic. 
+                        This project was inspired by a tweet from "Nuno Maduro" so I thought I would
+                        showcase my take
+                        on the topic.
 
-                    <br/> <br/>
-                    Technologies used.
+                        <br /> <br />
+                        Technologies used.
 
-                    <br/> <br/>
-                    Source Code
-                </p>
-            </div>
+                        <br /> <br />
+                        Source Code
+                    </p>
+                </div>
+            </main>
         </>
     );
 }
