adc3e9814c644b6230f9f6f2824df684056e4251
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-19 19:24:04 +0200

    edit: add functionality

--- src/views/authenticated/Home.jsx
+++ src/views/authenticated/Home.jsx
@@ -1,6 +1,36 @@
 import AuthenticatedNavbar from '../../components/AuthenticatedNavbar';
-import Sidebar from '../../components/Sidebar'
+import Sidebar from '../../components/Sidebar';
+import { useEffect, useState } from 'react';
+import apiClient from '../../api/axios';
+import { NavLink, useNavigate } from 'react-router';
+
 function Home() {
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    async function fetchData() {
+        const response = await apiClient.get('/api/task');
+        setData(response.data);
+        console.log(response.data);
+    }
+
+    async function deleteTask(id) {
+        await apiClient.delete(`/api/task/${id}`);
+        fetchData();
+    }
+
+    function viewTask(data) {
+        navigate('/authenticated/task-details', {
+            state: {
+                data
+            },
+        });
+    }
+
     return (
         <>
             <AuthenticatedNavbar />
@@ -10,8 +40,8 @@ function Home() {
                     <div className="border mx-lg-5 mb-5 mt-4 p-4">
                         <div className='d-flex align-items-center justify-content-between'>
                             <h4>Active</h4>
-                            <button type="button" className="btn btn-primary ms-3">
-                                Add Task</button></div>
+                            <NavLink to="/authenticated/create-task" className="btn btn-primary ms-3">
+                                Add Task</NavLink></div>
                         <p className="fw-lighter border-bottom">
                             Below you will find active records. You can create then delete records
                             which will be soft deleted and then moved to Trash.
@@ -25,24 +55,15 @@ function Home() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>School Work</td>
-                                    <td>Study for the mathematics test on wednesday.</td>
-                                    <td><button type="button" className="btn btn-primary">Edit</button></td>
-                                    <td><button type="button" className="btn btn-secondary">Delete</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Chores</td>
-                                    <td>Sweep the yard.</td>
-                                    <td><button type="button" className="btn btn-primary">Edit</button></td>
-                                    <td><button type="button" className="btn btn-secondary">Delete</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Gym</td>
-                                    <td>Upper body and cardio workout.</td>
-                                    <td><button type="button" className="btn btn-primary">Edit</button></td>
-                                    <td><button type="button" className="btn btn-secondary">Delete</button></td>
-                                </tr>
+                                {data?.map((data) => (
+                                    <tr key={data.id}>
+                                        <td onClick={() => viewTask(data)} className='hoverable'>{data.task_title}</td>
+                                        <td onClick={() => viewTask(data)} className='hoverable'>{data.task_definition}</td>
+                                        <td><NavLink to="/authenticated/edit-task" className="btn btn-primary"
+                                            state={data}>Edit</NavLink></td>
+                                        <td><button type="button" onClick={() => { deleteTask(data.id) }} className="btn btn-secondary">Trash</button></td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
