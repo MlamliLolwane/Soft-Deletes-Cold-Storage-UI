208bd8edb9e37be627d84bbe34eb0b4439d78002
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-19 22:42:46 +0200

    add: ui for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+import './App.css';
+import Typed from 'typed.js';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const typed = new Typed('#typed-text', {
+      strings: ['SOFT DELETES'],
+      typeSpeed: 100,
+      showCursor: false,
+      onComplete: () => {
+        setTimeout(() => {
+          const intro = document.getElementById('intro');
+          if (intro) {
+            intro.classList.remove('d-none');
+          }
+        }, 900);
+      },
+    });
+
+    document.getElementById('intro').removeAttribute('d-none');
+    return () => typed.destroy();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className='landing'>
+      <div className='flex-grow-1 mx-auto landing center border'>
+        <div className='center2'>
+          <p style={{ fontSize: "90px" }} className='mx-auto align-middle fw-bold pb-0 mb-0' id="typed-text"></p>
+          <h3 id="intro" className="text-center pt-0 mt-0 d-none">A project by Mlamli Lolwane </h3>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="container my-5">
+        <div className="row">
+          <div className="col">
+            <h5 className='pt-5 pb-3'>Hi and welcome to this demo project</h5>
+            <p className='fw-lighter'>
+              This project is the implementation of <u><a href='#' className="fw-lighter">
+                Soft Deletes - My Take</a></u> article I wrote on Medium. DO note that this is only a demo project
+              so code from this project should be used with caution.
+            </p>
+
+            <p className='fw-lighter'>
+              This project is a simple to do application where by users can create a to do item by inputing the
+              title of the task they have to complete and then add a description as well. Then users can mark the
+              status of the task (Complete / Cancelled).
+            </p>
+            <p className='fw-lighter'>
+              This project is made up of 2 "sub-projects", a backend powered by Laravel 12 and a frontend powered
+              by ReactJS.
+            </p>
+            <p className='fw-lighter'>
+              Code for both projects can be found on the following links: <br/>
+              <u><a href='#' className="fw-lighter">Laravel Backend</a></u> and <u><a href='#' className="fw-lighter">ReactJS Frontend</a></u>
+            </p>
+          </div>
+          <div className="col">
+            <div className="video-wrapper mb-5">
+              <div className="mb-5">
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/m6S1P9AIF84"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='border-top'></div>
+      <div className='text-center mb-3 mt-5'>
+        <p className='fw-lighter'>Made with <i class="bi bi-heart-fill"></i> by Mlamli Lolwane</p>
+      </div>
+      <br/>
+      </div>
     </>
   )
 }
