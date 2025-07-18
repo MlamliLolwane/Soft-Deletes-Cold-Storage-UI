663b7d64ab926adfacb920b0f396991c1c5dde86
Mlamli Lolwane <53498225+MlamliLolwane@users.noreply.github.com>
2025-07-18 05:05:15 +0200

    Update About.jsx

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -10,10 +10,10 @@ function About() {
                     <p className="fw-lighter">
                         Many developers debate whether using soft-deletes in a project is good or bad. My point of view is that
                         like all the other concepts in software development, there are no silver bullets... we use what we gotta use when
-                        we have to use them.
+                        we have to use them. <br/><br/>
                         This project demonstrates the implementation and benefits of soft deletes in modern web applications. Rather 
                         than permanently removing data from the database, soft deletes allow records to be marked as deleted while 
-                        still retaining them for future reference, recovery, or auditing purposes.<br/>
+                        still retaining them for future reference, recovery, or auditing purposes.<br<br/>
                         A simple to do application was used to demonstrate this where by users can create a task. After that the task
                         can be soft-deleted and will be sent to the "Trash". While the record is in the "Trash" it can then be permanently 
                         deleted which will not delete the task right away but after a specified amount of time to avoid loss of data in case
